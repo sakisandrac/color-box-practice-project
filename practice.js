d2485666e034734d7cc9cac86c591fd69094aaff
Sandra Chatphatthanasiri <sakisandra.c@gmail.com>
2023-01-23 13:18:00 -0700

    Added a third color

--- practice.js
+++ practice.js
@@ -1,11 +1,14 @@
 var button = document.querySelector('button');
 var box = document.getElementById('changeMe');
 
-button.onclick = function changeColor(){
-    if(box.style.background == 'lightpink'){
-        box.style.background = 'lightblue'}
-    else {
-        box.style.background = 'lightpink'
+function changeColor(){
+    if (box.style.background == 'lightpink'){
+        box.style.background = 'lightblue';
+    }else if (box.style.background == 'lightblue'){
+        box.style.background = 'purple';
+    } else {
+        box.style.background = 'lightpink';
     }
+    
 
 }
