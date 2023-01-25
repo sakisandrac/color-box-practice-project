8a82d5ac9c84074c9ea39fbc01cea8be98cf567f
Sandra Chatphatthanasiri <sakisandra.c@gmail.com>
2023-01-25 15:34:27 -0700

    Changed to swtich statement

--- practice.js
+++ practice.js
@@ -2,13 +2,26 @@ var button = document.querySelector('button');
 var box = document.getElementById('changeMe');
 
 function changeColor(){
-    if (box.style.background == 'lightpink'){
+    switch(box.style.background){
+        case 'lightpink':
         box.style.background = 'lightblue';
-    }else if (box.style.background == 'lightblue'){
-        box.style.background = 'purple';
-    } else {
-        box.style.background = 'lightpink';
+        break;
+
+        case 'lightblue':
+        box.style.background = 'aquamarine'
+        break;
+
+        case 'aquamarine':
+            box.style.background = 'grey'
+            break;
+
+        case 'grey':
+            box.style.background = 'lightpink'
+            break;
+
+        default:
+            box.innerHTML = 'what?'
+
     }
-    
 
 }
